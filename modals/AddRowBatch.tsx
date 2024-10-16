22ffc474cb7e9e151781e4a2a681dfeab788b1ab
shivapatel7 <shivapatel098k@gmail.com>
2024-10-16 15:52:58 -0700

    adding space to batchname in candidates

--- modals/AddRowBatch.tsx
+++ modals/AddRowBatch.tsx
@@ -317,18 +317,19 @@ const AddRowModal: React.FC<AddRowModalProps> = ({ isOpen, onClose, refreshData}
           />
         </div>
         <button
-            type="button"
-            onClick={onClose}
-            className="mt-2 w-full bg-gray-600 text-white py-2 rounded-lg hover:bg-gray-700 transition duration-200"
-          >
-            Cancel
-          </button>
-          <button
-            type="submit"
-            className="mt-4 w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition duration-200"
-          >
-            Save Batch
-          </button>
+        type="submit"
+        className="mt-4 w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition duration-200"
+      >
+        Save Batch
+      </button>
+
+      <button
+        type="button"
+        onClick={onClose}
+        className="mt-2 w-full bg-gray-600 text-white py-2 rounded-lg hover:bg-gray-700 transition duration-200"
+      >
+        Cancel
+      </button>
           </form>
       </div>
     </Modal>
